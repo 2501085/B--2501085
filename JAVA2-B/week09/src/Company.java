96eb0fc48b2daa874486b86a6a50880932960f6e
sj435 <Read25@kbu.ac.kr>
2026-06-11 15:21:03 +0900

    JAVA2B

---
+++ JAVA2-B/week09/src/Company.java
@@ -0,0 +1,15 @@
+public class Company {
+    private Employee[] employees;
+
+    public Company(Employee[] employees) {
+        this.employees = employees;
+    }
+
+
+
+    protected void sortByGross() {
+
+    }
+
+
+}
